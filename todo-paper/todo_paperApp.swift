4413c746ca01c9fa381c60d6754cd4865c67bdda
이지수 <127957826+jisulee154@users.noreply.github.com>
2023-07-10 03:25:33 +0900

    first commit

---
+++ todo-paper/todo_paperApp.swift
@@ -0,0 +1,30 @@
+//
+//  todo_paperApp.swift
+//  todo-paper
+//
+//  Created by 이지수 on 2023/07/07.
+//
+
+import SwiftUI
+
+@main
+struct todo_paperApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            TabView {
+                ContentView()
+                    .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                    .tabItem {
+                        Label("List", systemImage: "book")
+                    }
+                CalendarTabView()
+                    .tabItem {
+                        Label("Calendar", systemImage: "book")
+                        
+                    }
+            }
+        }
+    }
+}
